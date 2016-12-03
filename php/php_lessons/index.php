56352a847417f3888715b1d9382811b9a085e06d
que-sera <que.sera.sera.gdh@gmail.com>
2016-12-03 13:59:44 +0700

    Use buildinserver No.03 #9

---
+++ php/php_lessons/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// コメント
+# コメント
+/*
+コメント
+コメント
+コメント
+*/
+
+echo "hello from the TOP!";
+echo "hello from the TOP! again";
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <body>
+        <p>Hello World<?php echo "from PHP"; ?></p>
+    </body>
+</html>
