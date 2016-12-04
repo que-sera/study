299fe845fc6a477fb6154e2ca030212741c4c79b
que-sera <que.sera.sera.gdh@gmail.com>
2016-12-04 10:26:58 +0700

    Use difine No. 05 #19

--- php/php_lessons/index.php
+++ php/php_lessons/index.php
@@ -1,17 +1,13 @@
 <?php
 
-/*
-変数: データにつけるラベル
+// 定数: 変更されない値につけるラベル
 
-データ型:
-- 文字列：string
-- 数値 integer float
-- 論理値 boolean / true false
-- 配列
-- オブジェクト
-- null
-*/
 
-$msg = "hello from the TOP!";
-echo $msg;
-var_dump($msg);
+define("MY_EMAIL", "taguchi@dotinstall.com");
+
+echo MY_EMAIL;
+//MY_EMAIL = "hogehoge";
+
+var_dump(__LINE__);//10
+var_dump(__FILE__);
+var_dump(__DIR__);
