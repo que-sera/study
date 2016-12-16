56d69391fd213fc47c7cc5aafd57fec5090fd3a3
que-sera <que.sera.sera.gdh@gmail.com>
2016-12-17 03:04:57 +0700

    Use array No.13 #19

--- php/php_lessons/index.php
+++ php/php_lessons/index.php
@@ -1,16 +1,17 @@
 <?php
 
-// ループ処理
-// while
-// do...while
+// 配列
+// key value
 
-$i = 20;
-while ($i < 10) {
-    echo $i;
-    $i++;
-}
+$sales = array(
+    "taguchi" => 200,
+    "fkoji" => 800,
+    "dotinstall" => 600,
+);
 
-//do {
-//    echo $i;
-//    $i++;
-//} while ($i < 10);
+var_dump($sales["fkoji"]); // 800
+$sales["fkoji"] = 900;
+var_dump($sales["fkoji"]); //900
+
+$colors = ["red", "blue", "pink"];
+var_dump($colors[1]); // blue
