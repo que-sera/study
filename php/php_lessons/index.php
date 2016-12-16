5d8ecb5db3150d9b72aeeeb4cc526e1ed8961f5d
que-sera <que.sera.sera.gdh@gmail.com>
2016-12-16 11:19:05 +0700

    Use switch No. 10 #19

--- php/php_lessons/index.php
+++ php/php_lessons/index.php
@@ -1,31 +1,20 @@
 <?php
 
-// 真偽値
-/*
-# falseになる場合
-　文字列: 空、"0"
-  数値: 0, 0.0
-  論理値: false
-  配列: 要素の数が0
-  null
-  */
+// switch 条件分岐
 
+$signal = "green";
 
- 
- 
- 
- $x = 5;
- //if ($x){
-if ($x== true){
-     echo "great";
- }
-
-//三項演算子
-
-$max = ($a > $b) ? $a : $b ;
-
-if($a > $b){
-    $max = $a;
-}else{
-    $max = $b;
+switch ($signal){
+    case"red";
+        echo "stop!";
+        break;
+    case "blue";
+    case "green";
+    echo "go!";
+    break;
+    case "yellow";
+    echo "caution!";
+    break;
+    default;
+    echo "wrong signal";
 }
