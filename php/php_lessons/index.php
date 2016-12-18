424a024c876dd842b1cf74077c24e4c5d9be093d
que-sera <que.sera.sera.gdh@gmail.com>
2016-12-18 13:01:21 +0700

    Use foreach No. 14 #19

--- php/php_lessons/index.php
+++ php/php_lessons/index.php
@@ -3,15 +3,33 @@
 // 配列
 // key value
 
-$sales = array(
-    "taguchi" => 200,
-    "fkoji" => 800,
-    "dotinstall" => 600,
-);
 
-var_dump($sales["fkoji"]); // 800
-$sales["fkoji"] = 900;
-var_dump($sales["fkoji"]); //900
+//$sales = [
+//    "tagushi" => 200,
+//    "fkoji" => 800,
+//    "dotinstall" => 600,
+//];
 
-$colors = ["red", "blue", "pink"];
-var_dump($colors[1]); // blue
+//foreach ($sales as $key => $value) {
+//    echo"($key) $value";
+//}
+
+$colors = ["red","blue","pink"] ;
+    
+//foreach ($colors as $value) {
+//    echo"$value";
+//}
+
+// foreach if while for コロン構文
+
+foreach ($colors as $value):
+echo "$value";
+endforeach;
+
+?>
+<ul>
+    <?php foreach ($colors as $value) : ?>
+    <li><?php echo "$value"; ?></li>
+    <?php endforeach; ?>
+    </ul>
+</ul>
