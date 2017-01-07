1655ef380a28c1b92e4c44ac92273d3e97b56fe6
que-sera <que.sera.sera.gdh@gmail.com>
2017-01-07 21:47:40 +0700

    Use Function Manual No. 17 #19

--- php/php_lessons/index.php
+++ php/php_lessons/index.php
@@ -2,12 +2,18 @@
 
 // 関数
 
-$lang ="ruby";
+$x = 5.6;
+//echo ceil($x); //6
+//echo floor($x); // 5
+//echo round($x); // 6
+//echo rand(1, 10);
 
-function sayHi($name){
-    $lang = "php";
-    echo "hi! $name from $lang";
-}
+$s1 = "hello";
+$s2 = "Cat";
+//echo strlen($s1); //s
+//echo mb_strlen($s2); //2
+//printf("%s - %s -%.3f", $s1, $s2, $x);
 
-sayHi("Tom");
-var_dump($lang); //ruby
+$colors = ["red", "blue", "pink"];
+echo count($colors);
+echo implode("@",$colors);
