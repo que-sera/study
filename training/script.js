7ecf3d570450ffc30307adcc21dfe0741e7267ad
que-sera <que.sera.sera.gdh@gmail.com>
2017-12-17 12:58:47 +0700

    change js

--- training/script.js
+++ training/script.js
@@ -1,5 +1,4 @@
-<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
-<script type="text/javascript">
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js>
     $(function(){
     $("ul.menu li").hover(function(){
         $("ul.sub:not(:animated)", this).slideDown();
